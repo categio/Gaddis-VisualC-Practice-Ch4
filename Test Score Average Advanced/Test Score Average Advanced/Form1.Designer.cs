c7666f35db6f9e75c8a41a9e7ce92fcd5bee7888
Categio <categio1@gmail.com>
2022-02-09 16:06:01 -0800

    Test Score Average Proj original set up complete, functional, added the hotkey ability to clear and exit buttons. Ready to add new code from text.

--- Test Score Average Advanced/Test Score Average Advanced/Form1.Designer.cs
+++ Test Score Average Advanced/Test Score Average Advanced/Form1.Designer.cs
@@ -29,13 +29,165 @@ namespace Test_Score_Average_Advanced
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.aveGroupBox = new System.Windows.Forms.GroupBox();
+            this.test3TextBox = new System.Windows.Forms.TextBox();
+            this.test2TextBox = new System.Windows.Forms.TextBox();
+            this.test1TextBox = new System.Windows.Forms.TextBox();
+            this.avgOutputLabel = new System.Windows.Forms.Label();
+            this.avgLabel = new System.Windows.Forms.Label();
+            this.test3Label = new System.Windows.Forms.Label();
+            this.test2Label = new System.Windows.Forms.Label();
+            this.test1Label = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.aveGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // aveGroupBox
+            // 
+            this.aveGroupBox.Controls.Add(this.test3TextBox);
+            this.aveGroupBox.Controls.Add(this.test2TextBox);
+            this.aveGroupBox.Controls.Add(this.test1TextBox);
+            this.aveGroupBox.Controls.Add(this.avgOutputLabel);
+            this.aveGroupBox.Controls.Add(this.avgLabel);
+            this.aveGroupBox.Controls.Add(this.test3Label);
+            this.aveGroupBox.Controls.Add(this.test2Label);
+            this.aveGroupBox.Controls.Add(this.test1Label);
+            this.aveGroupBox.Location = new System.Drawing.Point(22, 24);
+            this.aveGroupBox.Name = "aveGroupBox";
+            this.aveGroupBox.Size = new System.Drawing.Size(254, 174);
+            this.aveGroupBox.TabIndex = 0;
+            this.aveGroupBox.TabStop = false;
+            this.aveGroupBox.Text = "Enter Three Test Scores";
+            // 
+            // test3TextBox
+            // 
+            this.test3TextBox.Location = new System.Drawing.Point(134, 98);
+            this.test3TextBox.Name = "test3TextBox";
+            this.test3TextBox.Size = new System.Drawing.Size(100, 20);
+            this.test3TextBox.TabIndex = 7;
+            // 
+            // test2TextBox
+            // 
+            this.test2TextBox.Location = new System.Drawing.Point(134, 66);
+            this.test2TextBox.Name = "test2TextBox";
+            this.test2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.test2TextBox.TabIndex = 6;
+            // 
+            // test1TextBox
+            // 
+            this.test1TextBox.Location = new System.Drawing.Point(134, 34);
+            this.test1TextBox.Name = "test1TextBox";
+            this.test1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.test1TextBox.TabIndex = 5;
+            // 
+            // avgOutputLabel
+            // 
+            this.avgOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.avgOutputLabel.Location = new System.Drawing.Point(134, 134);
+            this.avgOutputLabel.Name = "avgOutputLabel";
+            this.avgOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.avgOutputLabel.TabIndex = 4;
+            this.avgOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // avgLabel
+            // 
+            this.avgLabel.AutoSize = true;
+            this.avgLabel.Location = new System.Drawing.Point(60, 139);
+            this.avgLabel.Name = "avgLabel";
+            this.avgLabel.Size = new System.Drawing.Size(47, 13);
+            this.avgLabel.TabIndex = 3;
+            this.avgLabel.Text = "Average";
+            // 
+            // test3Label
+            // 
+            this.test3Label.AutoSize = true;
+            this.test3Label.Location = new System.Drawing.Point(32, 101);
+            this.test3Label.Name = "test3Label";
+            this.test3Label.Size = new System.Drawing.Size(75, 13);
+            this.test3Label.TabIndex = 2;
+            this.test3Label.Text = "Test Score #3";
+            // 
+            // test2Label
+            // 
+            this.test2Label.AutoSize = true;
+            this.test2Label.Location = new System.Drawing.Point(32, 69);
+            this.test2Label.Name = "test2Label";
+            this.test2Label.Size = new System.Drawing.Size(75, 13);
+            this.test2Label.TabIndex = 1;
+            this.test2Label.Text = "Test Score #2\r\n";
+            // 
+            // test1Label
+            // 
+            this.test1Label.AutoSize = true;
+            this.test1Label.Location = new System.Drawing.Point(32, 37);
+            this.test1Label.Name = "test1Label";
+            this.test1Label.Size = new System.Drawing.Size(75, 13);
+            this.test1Label.TabIndex = 0;
+            this.test1Label.Text = "Test Score #1";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(64, 215);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(81, 59);
+            this.calcButton.TabIndex = 1;
+            this.calcButton.Text = "Calculate Average";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(158, 215);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "&Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(158, 251);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(299, 289);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.aveGroupBox);
+            this.Name = "Form1";
+            this.Text = "Test Score Average Calculator";
+            this.aveGroupBox.ResumeLayout(false);
+            this.aveGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox aveGroupBox;
+        private System.Windows.Forms.TextBox test3TextBox;
+        private System.Windows.Forms.TextBox test2TextBox;
+        private System.Windows.Forms.TextBox test1TextBox;
+        private System.Windows.Forms.Label avgOutputLabel;
+        private System.Windows.Forms.Label avgLabel;
+        private System.Windows.Forms.Label test3Label;
+        private System.Windows.Forms.Label test2Label;
+        private System.Windows.Forms.Label test1Label;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- Test Score Average Advanced/Test Score Average Advanced/Form1.cs
+++ Test Score Average Advanced/Test Score Average Advanced/Form1.cs
@@ -16,5 +16,46 @@ namespace Test_Score_Average_Advanced
         {
             InitializeComponent();
         }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double test1; //holds test one score
+                double test2; //holds test two score
+                double test3; //holds test three score
+                double average; //holds the average score
+                //collect user entered scores
+                test1 = double.Parse(test1TextBox.Text);
+                test2 = double.Parse(test2TextBox.Text);
+                test3 = double.Parse(test3TextBox.Text);
+                //calculate average from user collected scores
+                average = (test1 + test2 + test3) / 3.0;
+                //display avg test score, output rounded to 1 decimal pt.
+                avgOutputLabel.Text = average.ToString("n1");
+            }
+            catch (Exception ex)
+            {
+                //display the default error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //clear all the data entered and printed
+            test1TextBox.Text = "";
+            test2TextBox.Text = "";
+            test3TextBox.Text = "";
+            avgOutputLabel.Text = "";
+        }
+
+        private void exitButton_Click_1(object sender, EventArgs e)
+        {
+            //close program/form
+            this.Close();
+        }
+
     }
 }
+
