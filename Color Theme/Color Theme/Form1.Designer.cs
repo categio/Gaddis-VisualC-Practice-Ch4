9d6103cd1fa4b849e10db2a22b3399f230458e9f
Categio <categio1@gmail.com>
2022-02-13 00:14:46 -0800

    created program showcasing radio button functionality. clicking the buttons change the corresponding background color of the form.

---
+++ Color Theme/Color Theme/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace Color_Theme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorGroupBox = new System.Windows.Forms.GroupBox();
+            this.yellowRadioButton = new System.Windows.Forms.RadioButton();
+            this.redRadioButton = new System.Windows.Forms.RadioButton();
+            this.whiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.normalRadioButton = new System.Windows.Forms.RadioButton();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.colorGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // colorGroupBox
+            // 
+            this.colorGroupBox.Controls.Add(this.normalRadioButton);
+            this.colorGroupBox.Controls.Add(this.whiteRadioButton);
+            this.colorGroupBox.Controls.Add(this.redRadioButton);
+            this.colorGroupBox.Controls.Add(this.yellowRadioButton);
+            this.colorGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorGroupBox.Location = new System.Drawing.Point(39, 15);
+            this.colorGroupBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.colorGroupBox.Name = "colorGroupBox";
+            this.colorGroupBox.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.colorGroupBox.Size = new System.Drawing.Size(250, 162);
+            this.colorGroupBox.TabIndex = 0;
+            this.colorGroupBox.TabStop = false;
+            this.colorGroupBox.Text = "Select a Background Color";
+            // 
+            // yellowRadioButton
+            // 
+            this.yellowRadioButton.AutoSize = true;
+            this.yellowRadioButton.Location = new System.Drawing.Point(75, 31);
+            this.yellowRadioButton.Name = "yellowRadioButton";
+            this.yellowRadioButton.Size = new System.Drawing.Size(66, 20);
+            this.yellowRadioButton.TabIndex = 0;
+            this.yellowRadioButton.TabStop = true;
+            this.yellowRadioButton.Text = "Yellow";
+            this.yellowRadioButton.UseVisualStyleBackColor = true;
+            this.yellowRadioButton.CheckedChanged += new System.EventHandler(this.yellowRadioButton_CheckedChanged);
+            // 
+            // redRadioButton
+            // 
+            this.redRadioButton.AutoSize = true;
+            this.redRadioButton.Location = new System.Drawing.Point(75, 59);
+            this.redRadioButton.Name = "redRadioButton";
+            this.redRadioButton.Size = new System.Drawing.Size(52, 20);
+            this.redRadioButton.TabIndex = 1;
+            this.redRadioButton.TabStop = true;
+            this.redRadioButton.Text = "Red";
+            this.redRadioButton.UseVisualStyleBackColor = true;
+            this.redRadioButton.CheckedChanged += new System.EventHandler(this.redRadioButton_CheckedChanged);
+            // 
+            // whiteRadioButton
+            // 
+            this.whiteRadioButton.AutoSize = true;
+            this.whiteRadioButton.Location = new System.Drawing.Point(75, 89);
+            this.whiteRadioButton.Name = "whiteRadioButton";
+            this.whiteRadioButton.Size = new System.Drawing.Size(60, 20);
+            this.whiteRadioButton.TabIndex = 2;
+            this.whiteRadioButton.TabStop = true;
+            this.whiteRadioButton.Text = "White";
+            this.whiteRadioButton.UseVisualStyleBackColor = true;
+            this.whiteRadioButton.CheckedChanged += new System.EventHandler(this.whiteRadioButton_CheckedChanged);
+            // 
+            // normalRadioButton
+            // 
+            this.normalRadioButton.AutoSize = true;
+            this.normalRadioButton.Location = new System.Drawing.Point(75, 118);
+            this.normalRadioButton.Name = "normalRadioButton";
+            this.normalRadioButton.Size = new System.Drawing.Size(118, 20);
+            this.normalRadioButton.TabIndex = 3;
+            this.normalRadioButton.TabStop = true;
+            this.normalRadioButton.Text = "Back to Normal";
+            this.normalRadioButton.UseVisualStyleBackColor = true;
+            this.normalRadioButton.CheckedChanged += new System.EventHandler(this.normalRadioButton_CheckedChanged);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(123, 196);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 235);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.colorGroupBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Color Theme";
+            this.colorGroupBox.ResumeLayout(false);
+            this.colorGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox colorGroupBox;
+        private System.Windows.Forms.RadioButton normalRadioButton;
+        private System.Windows.Forms.RadioButton whiteRadioButton;
+        private System.Windows.Forms.RadioButton redRadioButton;
+        private System.Windows.Forms.RadioButton yellowRadioButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
